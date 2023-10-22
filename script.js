8099028f3156f8eb8e6a7de7a10a6185d85a10c0
toiletsplurge <MrTakeTooMuch@gmail.com>
2023-10-22 12:51:35 -0400

    Improved logic and styling

--- script.js
+++ script.js
@@ -1,61 +1,22 @@
-const ageInYearsPlaceholder = document.getElementById("yearsPlaceholder");
-const ageInMonthsPlaceholder = document.getElementById("monthsPlaceholder");
-const ageInDaysPlaceholder = document.getElementById("daysPlaceholder");
+myFunction = (event) => {
+  event.preventDefault();
 
-function validateDate(day, month, year) {
-  const date = new Date();
-  const currentDay = date.getDate();
-  const currentMonth = date.getMonth() + 1;
-  const currentYear = date.getFullYear();
+  const day = document.getElementById("day").value;
+  const month = document.getElementById("month").value;
+  const year = document.getElementById("year").value;
 
-  if (
-    isNaN(day) ||
-    isNaN(month) ||
-    isNaN(year) ||
-    day < 1 ||
-    day > 31 ||
-    month < 1 ||
-    month > 12 ||
-    year > currentYear ||
-    (year === currentYear && month > currentMonth) ||
-    (year === currentYear && month === currentMonth && day > currentDay)
-  ) {
-    return false;
-  }
-  const daysInMonth = new Date(year, month, 0).getDate();
+  const currentDate = new Date();
 
-  return day <= daysInMonth;
-}
+  const birthDate = new Date(year, month - 1, day);
 
-//calculating current day
-function ageCalculator() {
-  const day = document.getElementById("day");
-  const month = document.getElementById("month");
-  const year = document.getElementById("year");
-
-  if (day === "" || month === "" || year === "") {
-    alert("Please enter all the values");
-    return;
-  }
-
-  const dayValue = parseInt(day, 10);
-  const monthValue = parseInt(month, 10);
-  const yearValue = parseInt(year, 10);
-
-  if (!validateDate(dayValue, monthValue, yearValue)) {
-    alert("Invalid date format!");
-    return;
-  }
-
-  //age in years, months, and days
-  let ageInYears = currentYear - yearValue;
-  let ageInMonths = currentMonth - monthValue;
-  let ageInDays = currentDay - dayValue;
+  let ageInYears = currentDate.getFullYear() - birthDate.getFullYear();
+  let ageInMonths = currentDate.getMonth() - birthDate.getMonth();
+  let ageInDays = currentDate.getDate() - birthDate.getDate();
 
   if (ageInDays < 0) {
     const daysInLastMonth = new Date(
-      currentYear,
-      currentMonth - 1,
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
       0
     ).getDate();
     ageInDays += daysInLastMonth;
@@ -69,25 +30,20 @@ function ageCalculator() {
 
   if (ageInDays < 0 && ageInMonths === 0) {
     const daysInLastMonth = new Date(
-      currentYear,
-      currentMonth - 1,
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
       0
     ).getDate();
     ageInDays += daysInLastMonth;
     ageInMonths = 11;
   }
 
-  let currentAge = `${ageInYears} years, ${ageInMonths} months, and ${ageInDays} days`;
+  // Update the placeholders with the calculated values
+  yearsPlaceholder.textContent = ageInYears;
+  monthsPlaceholder.textContent = ageInMonths;
+  daysPlaceholder.textContent = ageInDays;
+};
 
-  //append the current age to the DOM
-  ageInYearsPlaceholder.textContent = ageInYears;
-  ageInMonthsPlaceholder.textContent = ageInMonths;
-  ageInDaysPlaceholder.textContent = ageInDays;
-
-  console.log(currentAge);
-}
-
-ageCalc.addEventListener("submit", function (event) {
-  event.preventDefault();
-  ageCalculator();
-});
+// Attach the event listener to the form
+const ageCalcForm = document.getElementById("ageCalc");
+ageCalcForm.addEventListener("submit", myFunction);
